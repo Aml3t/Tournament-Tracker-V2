90213b75757c5dff0bde160f16eb1cb91180f314
Aml3t <fischerparsos@gmail.com>
2023-05-30 14:58:38 +0300

    Editing the Index

---
+++ TrackerLibrary/MVCUI/Controllers/PeopleController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrackerLibrary;
+using TrackerLibrary.Models;
+
+namespace MVCUI.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            List<PersonModel> availablePeople = GlobalConfig.Connection.GetPerson_All();
+
+            return View(availablePeople);
+        }
+
+        // GET: People/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: People/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
