812e555672f55dd672c2f27bbbb70adda94429b2
Aml3t <fischerparsos@gmail.com>
2023-05-31 13:04:48 +0300

    Making visual editing for the forms

--- TrackerLibrary/TrackerLibrary/Models/PersonModel.cs
+++ TrackerLibrary/TrackerLibrary/Models/PersonModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace TrackerLibrary.Models
@@ -7,9 +9,17 @@ namespace TrackerLibrary.Models
     public class PersonModel
     {
         public int Id { get; set; }
+
+        [Display(Name = "First Name")]
         public String FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
         public String LastName { get; set; }
+
+        [Display(Name = "Email Address")]
         public String EmailAddress { get; set; }
+
+        [Display(Name = "Mobile number")]
         public String CellphoneNumber { get; set; }
 
         public string FullName
