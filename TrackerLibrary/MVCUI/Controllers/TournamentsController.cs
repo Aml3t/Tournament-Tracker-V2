de38a157dc23406e3d7099010b2330171c9808c2
Aml3t <fischerparsos@gmail.com>
2023-06-07 12:16:05 +0300

    Tournament Form

---
+++ TrackerLibrary/MVCUI/Controllers/TournamentsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrackerLibrary;
+using TrackerLibrary.Models;
+
+namespace MVCUI.Controllers
+{
+    public class TournamentsController : Controller
+    {
+        // GET: Tournaments
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tournaments/Create
+        [ValidateAntiForgeryToken()]
+        [HttpPost]
+        public ActionResult Create(TournamentModel p)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    // TODO - Create the tournament
+                    //GlobalConfig.Connection.CreateTournament(p);
+                    
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
