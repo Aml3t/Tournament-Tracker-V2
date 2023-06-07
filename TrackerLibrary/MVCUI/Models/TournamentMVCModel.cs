506a1ad7eff952db63ba4b04418e711c99b1f781
Aml3t <fischerparsos@gmail.com>
2023-06-07 12:50:03 +0300

    Tournament Form

---
+++ TrackerLibrary/MVCUI/Models/TournamentMVCModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrackerLibrary.Models;
+
+namespace MVCUI.Models
+{
+    public class TournamentMVCModel
+    {
+        [DisplayName("Tournament Name")]
+        [Required]
+        [StringLength(100,MinimumLength = 2)]
+        public string TournamentName { get; set; }
+
+        [DisplayName("Entry Fee")]
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal EntryFee { get; set; }
+
+        [DisplayName("Entered Teams")]
+        public List<SelectListItem> EnteredTeams { get; set; } = new List<SelectListItem>();
+
+        public List<string> SelectedEnteredTeams { get; set; } = new List<string>();
+
+        [DisplayName("Prizes")]
+        public List<SelectListItem> Prizes { get; set; } = new List<SelectListItem>();
+
+        public List<string> SelectedPrizes { get; set; } = new List<string>();
+    }
+
+}
